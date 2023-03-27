f05d46c5f35c793841057b7bc2d074b5d97bd0de
hapem85 <hapem85@gmail.com>
2023-03-27 11:38:12 -0500

    Initiate spring boot application for crypto trading system

---
+++ src/main/java/com/crypto/trading/config/CryptoTradingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.crypto.trading.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingSystemApplication.class, args);
+	}
+}
